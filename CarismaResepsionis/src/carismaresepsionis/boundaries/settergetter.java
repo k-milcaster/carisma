f3bbd72a2c0a9646e1c66c8b31f43277122dafca
fiqhidarmawan <fiqhidarmawan@ymail.com>
2015-04-05 12:29:54 +0700

    GUI-Devan

---
+++ CarismaResepsionis/src/carismaresepsionis/boundaries/settergetter.java
@@ -0,0 +1,173 @@
+
+package carismaresepsionis.boundaries;
+
+public class settergetter {
+   int tinggi,bert,umur;
+   String id,nama,tempat,bulanlahir,tanggal,tahun,hp,telp,kk,klain,aamat,provinsi,kota,jeniskelamin,goldar,kamar,kelaskamar;
+
+    public String getKamar() {
+        return kamar;
+    }
+
+    public void setKamar(String kamar) {
+        this.kamar = kamar;
+    }
+
+    public String getKelaskamar() {
+        return kelaskamar;
+    }
+
+    public void setKelaskamar(String kelaskamar) {
+        this.kelaskamar = kelaskamar;
+    }
+
+    public void setTinggi(int tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    public void setBert(int bert) {
+        this.bert = bert;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setTempat(String tempat) {
+        this.tempat = tempat;
+    }
+
+    public void setBulanlahir(String bulanlahir) {
+        this.bulanlahir = bulanlahir;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public void setTahun(String tahun) {
+        this.tahun = tahun;
+    }
+
+    public void setHp(String hp) {
+        this.hp = hp;
+    }
+
+    public void setTelp(String telp) {
+        this.telp = telp;
+    }
+
+    public void setKk(String kk) {
+        this.kk = kk;
+    }
+
+    public void setKlain(String klain) {
+        this.klain = klain;
+    }
+
+    public void setAamat(String aamat) {
+        this.aamat = aamat;
+    }
+
+    public void setProvinsi(String provinsi) {
+        this.provinsi = provinsi;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public void setJeniskelamin(String jeniskelamin) {
+        this.jeniskelamin = jeniskelamin;
+    }
+
+    public void setGoldar(String goldar) {
+        this.goldar = goldar;
+    }
+
+    public int getTinggi() {
+        return tinggi;
+    }
+
+    public int getBert() {
+        return bert;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getTempat() {
+        return tempat;
+    }
+
+    public String getBulanlahir() {
+        return bulanlahir;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public String getTahun() {
+        return tahun;
+    }
+
+    public String getHp() {
+        return hp;
+    }
+
+    public String getTelp() {
+        return telp;
+    }
+
+    public String getKk() {
+        return kk;
+    }
+
+    public String getKlain() {
+        return klain;
+    }
+
+    public String getAamat() {
+        return aamat;
+    }
+
+    public String getProvinsi() {
+        return provinsi;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public String getJeniskelamin() {
+        return jeniskelamin;
+    }
+
+    public String getGoldar() {
+        return goldar;
+    }
+   
+    
+    
+    
+    
+    
+}
