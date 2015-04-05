d9a810be567bc2133d5f3c47dfabbe3f37a5f525
Fitriana Dzulfaidah <fitrianadz94@gmail.com>
2015-04-05 11:10:39 +0700

    GUI Fitdzul

---
+++ CarismaApoteker/src/carismaapoteker/boundaries/TabelObat.java
@@ -0,0 +1,170 @@
+package carismaapoteker.boundaries;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.util.ArrayList;
+
+public class TabelObat extends javax.swing.JFrame {
+
+    private TransaksiJualObat ui;
+    private ArrayList<String> idObat = new ArrayList<>();
+    private ArrayList<String> namaObat = new ArrayList<>();
+    private ArrayList<String> hargaObat = new ArrayList<>();
+    
+    public TabelObat(TransaksiJualObat ui) {
+        initComponents();
+        this.ui = ui;
+        setLocationRelativeTo(ui);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTableObat = new javax.swing.JTable();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Daftar Obat");
+
+        jTableObat.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"OB08980", "Aspirin", "7000"},
+                {"OB08978", "Paracetamol", "8000"},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Id Obat", "Nama", "harga"
+            }
+        ));
+        jTableObat.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTableObatMouseClicked(evt);
+            }
+        });
+        jScrollPane2.setViewportView(jTableObat);
+
+        jTextField1.setFont(new java.awt.Font("Tahoma", 2, 11)); // NOI18N
+        jTextField1.setForeground(new java.awt.Color(204, 204, 204));
+        jTextField1.setText("cari obat");
+        jTextField1.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                jTextField1FocusLost(evt);
+            }
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jTextField1FocusGained(evt);
+            }
+        });
+        jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTextField1KeyReleased(evt);
+            }
+        });
+
+        jButton1.setText("OK");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jTableObatMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableObatMouseClicked
+        int row = jTableObat.getSelectedRow();
+        String id = String.valueOf(jTableObat.getValueAt(row, 0));
+        String nama = String.valueOf(jTableObat.getValueAt(row, 1));
+        String harga = String.valueOf(jTableObat.getValueAt(row, 2));
+        
+        idObat.add(id);
+        namaObat.add(nama);
+        hargaObat.add(harga);
+    }//GEN-LAST:event_jTableObatMouseClicked
+
+    private void jTextField1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField1FocusGained
+        if (jTextField1.getText().equals("cari obat")) {
+            jTextField1.setText("");
+        }
+    }//GEN-LAST:event_jTextField1FocusGained
+
+    private void jTextField1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField1FocusLost
+        if (jTextField1.getText().equals("")) {
+            jTextField1.setText("cari obat");
+            jTextField1.setFont(new Font("Tahoma", 0,11 ));
+            jTextField1.setForeground(Color.gray);
+        }
+    }//GEN-LAST:event_jTextField1FocusLost
+
+    private void jTextField1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyReleased
+        jTextField1.setForeground(Color.black);
+    }//GEN-LAST:event_jTextField1KeyReleased
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        int count = idObat.size();
+        for (int i = 0; i < count; i++) {
+            ui.jTableOfSales.setValueAt(idObat.get(i), ui.row, 0);
+            ui.jTableOfSales.setValueAt(namaObat.get(i), ui.row, 1);
+            ui.jTableOfSales.setValueAt(hargaObat.get(i), ui.row, 3);
+            ui.row ++;
+        }
+        
+        
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTableObat;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
