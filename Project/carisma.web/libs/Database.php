b902a2ac5832d9badcea3eb93c81e08a8346c6ad
devanastiko <devan.astiko@gmail.com>
2015-04-10 17:48:39 +0700

    Do : Tambah Halaman Error Hambatan : tidak pernah ada To do : getJadwalDokter

--- Project/carisma.web/libs/Database.php
+++ Project/carisma.web/libs/Database.php
@@ -3,7 +3,11 @@
 class Database extends PDO{
 
     function __construct() {
-        parent::__construct('mysql:host=127.0.0.1:3306;dbname=codeducation_web', 'root', '');
+        parent::__construct('mysql:host=127.0.0.1:3306;dbname=carisma', 'root', '');
     }
 
+
+    
+    
+    
 }
