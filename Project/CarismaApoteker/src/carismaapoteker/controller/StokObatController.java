2ee27f0842dbe2a9a3fe4bb2b32d109233ff68f8
hanamahrifah <hanamahrifah@gmail.com>
2015-04-16 15:01:11 +0700

    Do : Update Stok Kritis pada tabel stok obat Hambatan : - To Do : melanjutkan skenario lain

--- Project/CarismaApoteker/src/carismaapoteker/controller/StokObatController.java
+++ Project/CarismaApoteker/src/carismaapoteker/controller/StokObatController.java
@@ -13,7 +13,6 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
-
 /**
  *
  * @author Hana Mahrifah
@@ -37,11 +36,18 @@ public class StokObatController {
         model.addColumn("Harga Jual");
         model.addColumn("Stok Kritis");
         for (int i = 0; i < list.size(); i++) {
-            model.addRow(new Object[]{list.get(i).getIdObat(), list.get(i).getNamaObat(), list.get(i).getQtyObat(), list.get(i).getJenisObat(), list.get(i).getKeterangan(), list.get(i).getHargajualObat(),list.get(i).getKeterangan()});
+            model.addRow(new Object[]{list.get(i).getIdObat(), list.get(i).getNamaObat(), list.get(i).getQtyObat(), list.get(i).getJenisObat(), list.get(i).getKeterangan(), list.get(i).getHargajualObat(),list.get(i).getStokkritisObat()});
             System.out.println("lewat");
         }
         ui.tableMedicine.setModel(model);
         return model;
+    }
+    public void updateStokObat (int idObat, int stok) throws RemoteException{   
+            Obat obat = new Obat();
+            obat.setIdObat(idObat);
+            obat.setStokkritisObat(stok);
+            obatService.updateObat(obat);
+    
     }
        
     }
