7752e8c66bb698df23d0edc92f0f2fc33fa49e48
Anes Dirsi Eka Permatasari <Annesdirsy@gmail.com>
2015-04-18 12:04:01 +0700

    do: mengubah nama variable di gui jadwaldokter hambatan: tidak ada To do: menyelesaikan skenario berikutnya

--- Project/CarismaResepsionis/src/carismaresepsionis/boundaries/jadwaldokter.java
+++ Project/CarismaResepsionis/src/carismaresepsionis/boundaries/jadwaldokter.java
@@ -1,6 +1,7 @@
 package carismaresepsionis.boundaries;
 
 import carismaresepsionis.controller.ClientSocket;
+import javax.swing.DefaultListModel;
 
 /**
  *
@@ -25,8 +26,8 @@ public class jadwaldokter extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox();
-        jComboBox2 = new javax.swing.JComboBox();
+        comboBoxNamaPoli = new javax.swing.JComboBox();
+        comboBoxNamaDokter = new javax.swing.JComboBox();
         jPanel2 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
@@ -59,9 +60,17 @@ public class jadwaldokter extends javax.swing.JFrame {
 
         jLabel4.setText("Nama");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Gigi dan Mulut", "Umum" }));
+        comboBoxNamaPoli.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboBoxNamaPoliActionPerformed(evt);
+            }
+        });
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Dr. Elly", "Dr. Hendrik" }));
+        comboBoxNamaDokter.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboBoxNamaDokterActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -76,8 +85,9 @@ public class jadwaldokter extends javax.swing.JFrame {
                 .addGap(53, 53, 53)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 352, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(comboBoxNamaPoli, javax.swing.GroupLayout.Alignment.LEADING, 0, 97, Short.MAX_VALUE)
+                        .addComponent(comboBoxNamaDokter, javax.swing.GroupLayout.Alignment.LEADING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap(41, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -86,10 +96,10 @@ public class jadwaldokter extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(comboBoxNamaPoli, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(comboBoxNamaDokter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -301,6 +311,11 @@ public class jadwaldokter extends javax.swing.JFrame {
 
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/1426718664_circle_back_arrow_-24.png"))); // NOI18N
         jButton1.setText("Back");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton1);
         jButton1.setBounds(830, 310, 90, 40);
 
@@ -323,10 +338,35 @@ public class jadwaldokter extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField13ActionPerformed
 
+    private void comboBoxNamaPoliActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboBoxNamaPoliActionPerformed
+        DefaultListModel model = new DefaultListModel();
+        for (int i = 0; i < comboBoxNamaPoli.getModel().getSize(); i++) {
+            Object item = comboBoxNamaPoli.getModel().getElementAt(i);
+            model.addElement(item.toString());
+        }
+        //int poli = comboBoxNamaPoli.getSelectedItem();
+        //model.addElement(poli);
+        
+    }//GEN-LAST:event_comboBoxNamaPoliActionPerformed
+
+    private void comboBoxNamaDokterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboBoxNamaDokterActionPerformed
+       DefaultListModel model = new DefaultListModel();
+        for (int i = 0; i < comboBoxNamaDokter.getModel().getSize(); i++) {
+            Object item = comboBoxNamaDokter.getModel().getElementAt(i);
+            model.addElement(item.toString());
+        }
+        //int poli = comboBoxNamaDokter.getSelectedItem();
+        //model.addElement(poli);
+    }//GEN-LAST:event_comboBoxNamaDokterActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JComboBox comboBoxNamaDokter;
+    public javax.swing.JComboBox comboBoxNamaPoli;
     private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JComboBox jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
