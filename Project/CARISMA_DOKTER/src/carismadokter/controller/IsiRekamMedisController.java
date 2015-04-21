783b0c5f3fb156106f82c6faaafd6ad3481b3d3b
Fitriana Dzulfaidah <fitrianadz94@gmail.com>
2015-04-21 11:17:48 +0700

    Do : Fix Insert Kunjungan Part 2 Hambatan : tidak ada To Do : Melanjutkan use case 2

--- Project/CARISMA_DOKTER/src/carismadokter/controller/IsiRekamMedisController.java
+++ Project/CARISMA_DOKTER/src/carismadokter/controller/IsiRekamMedisController.java
@@ -3,10 +3,12 @@
  * and open the template in the editor.
  */
 package carismadokter.controller;
+
 import carismadokter.boundaries.IsiResep;
 import carismadokter.boundaries.isirekammedis;
 import carismainterface.entity.Detailresep;
 import carismainterface.entity.Dokter;
+import carismainterface.entity.Kunjungan;
 import carismainterface.entity.Obat;
 import carismainterface.entity.Pasien;
 import carismainterface.entity.Penyakit;
@@ -16,6 +18,7 @@ import carismainterface.entity.Resep;
 import carismainterface.entity.User;
 import carismainterface.server.DetailresepService;
 import carismainterface.server.DokterService;
+import carismainterface.server.KunjunganService;
 import carismainterface.server.ObatService;
 import carismainterface.server.PasienService;
 import carismainterface.server.PenyakitService;
@@ -28,13 +31,13 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author User
  */
-
 public class IsiRekamMedisController {
-    
+
     private RekammedikService rekamMedikService;
     private RekammedikpenyakitService rekamMedisPenyakitService;
     private ResepService resepService;
@@ -43,9 +46,9 @@ public class IsiRekamMedisController {
     private DokterService dokterService;
     private DetailresepService detailResepService;
     private PasienService pasienService;
-    
-    
-    public IsiRekamMedisController(ClientSocket client) throws RemoteException{
+    private KunjunganService kunjunganService;
+
+    public IsiRekamMedisController(ClientSocket client) throws RemoteException {
         rekamMedikService = client.getRekamMedikService();
         rekamMedisPenyakitService = client.getRekamMedisPenyakitService();
         resepService = client.getResepService();
@@ -54,9 +57,10 @@ public class IsiRekamMedisController {
         dokterService = client.getDokterService();
         detailResepService = client.getDetailResepService();
         pasienService = client.getPasienService();
+        kunjunganService = client.getKunjunganService();
     }
-    
-    public void insertRekamMedis(String idRekammedik, String idDokter, String idPasien, String tglRekamMedis, String keluhan, String pemeriksaan, String terapi, String alergiObat, String kesimpulanPemeriksaan, String kondisiPasien, String idResep) throws RemoteException{
+
+    public void insertRekamMedis(String idRekammedik, String idDokter, String idPasien, String tglRekamMedis, String keluhan, String pemeriksaan, String terapi, String alergiObat, String kesimpulanPemeriksaan, String kondisiPasien, String idResep) throws RemoteException {
         Rekammedik rekamMedik = new Rekammedik();
         rekamMedik.setIdRekammedik(idRekammedik);
         rekamMedik.setDokterIdDokter(idDokter);
@@ -71,20 +75,20 @@ public class IsiRekamMedisController {
         rekamMedik.setResepIdResep(idResep);
         rekamMedikService.insertRekamMedik(rekamMedik);
     }
-    
-    public void insertRekamMedisPenyakit(String idRekamMedis, String idPenyakit) throws RemoteException{
+
+    public void insertRekamMedisPenyakit(String idRekamMedis, String idPenyakit) throws RemoteException {
         Rekammedikpenyakit rekamMedisPenyakit = new Rekammedikpenyakit(idRekamMedis, idPenyakit);
         rekamMedisPenyakitService.insertRekamMedikPenyakit(rekamMedisPenyakit);
     }
-     
-    public void insertResep(String idResep, String keterangan) throws RemoteException{
+
+    public void insertResep(String idResep, String keterangan) throws RemoteException {
         Resep resep = new Resep();
         resep.setIdResep(idResep);
         resep.setKeterangan(keterangan);
         resepService.insertResep(resep);
     }
-    
-    public void insertDetailResep(String idDetailResep, String idResep, String namaObat, int quantity, String aturanPakai)throws RemoteException{
+
+    public void insertDetailResep(String idDetailResep, String idResep, String namaObat, int quantity, String aturanPakai) throws RemoteException {
         Detailresep detailResep = new Detailresep();
         detailResep.setIdDetailresep(idDetailResep);
         detailResep.setResepIdResep(idResep);
@@ -94,83 +98,90 @@ public class IsiRekamMedisController {
         detailResepService.insertDetailresep(detailResep);
     }
     
-    public String getIdDetailResep() throws RemoteException{
+    public void insertKunjungan(String idKunjungan, String idPasien, String idRekamMedik, String tglKunjungan, int biaya) throws RemoteException{
+        Kunjungan kunjungan = new Kunjungan();
+        kunjungan.setIdKunjungan(idKunjungan);
+        kunjungan.setPasienIdPasien(idPasien);
+        kunjungan.setRekammedikIdRekammedik(idRekamMedik);
+        kunjungan.setTransaksijualobatIdTransaksijual("kosong");
+        kunjungan.setPasienKamarIdPeminjaman("kosong");
+        kunjungan.setTanggaljamKunjungan(tglKunjungan);
+        kunjungan.setBiayaKunjungan(biaya);
+        kunjunganService.insertKunjungan(kunjungan);
+    }
+
+    public String getIdDetailResep() throws RemoteException {
         String lastIdidDetailResep = detailResepService.getLastIdDetailResep();
         String awalan = "DETRES-".concat(getDateNow().concat("-"));
         //DETRES-2015-04-14-001
-        
+
         String idDetailResepFix = " ";
-        
+
         String getDateOnly = " ";
         if (lastIdidDetailResep != null) {
             char[] charDate = lastIdidDetailResep.toCharArray();
             char[] newCharDate = new char[10];
             for (int i = 0; i < 10; i++) {
-                newCharDate[i] = charDate[i+7];
+                newCharDate[i] = charDate[i + 7];
             }
             getDateOnly = String.valueOf(newCharDate);
         }
         if (lastIdidDetailResep == null || (!getDateNow().equals(getDateOnly))) {
             idDetailResepFix = awalan.concat("001");
-        }
-        else{
+        } else {
             char[] lastDigit = lastIdidDetailResep.toCharArray();
             char[] newLastDigit = new char[3];
             for (int i = 0; i < 3; i++) {
-                newLastDigit[i] = lastDigit[i+18];
+                newLastDigit[i] = lastDigit[i + 18];
             }
-            int lastDigitIdDetail = Integer.parseInt(String.valueOf(newLastDigit))+1;
+            int lastDigitIdDetail = Integer.parseInt(String.valueOf(newLastDigit)) + 1;
             if (lastDigitIdDetail < 10) {
                 idDetailResepFix = awalan.concat("00").concat(String.valueOf(lastDigitIdDetail));
-            }
-            else if (lastDigitIdDetail >= 10 && lastDigitIdDetail < 100){
+            } else if (lastDigitIdDetail >= 10 && lastDigitIdDetail < 100) {
                 idDetailResepFix = awalan.concat("0").concat(String.valueOf(lastDigitIdDetail));
-            }
-            else if (lastDigitIdDetail >= 100){
+            } else if (lastDigitIdDetail >= 100) {
                 idDetailResepFix = awalan.concat("").concat(String.valueOf(lastDigitIdDetail));
             }
         }
         return idDetailResepFix;
     }
-    
-    public String getIdResep() throws RemoteException{
+
+    public String getIdResep() throws RemoteException {
         String lastIdResep = resepService.getLastIdResep();
         String awalan = "RES-".concat(getDateNow().concat("-"));
         //RES-2015-04-14-001
         String idResepFix = " ";
         String getDateOnly = " ";
-            if (lastIdResep != null) {
-                char[] charDate = lastIdResep.toCharArray();
-                char[] newCharDate = new char[10];
-                for (int i = 0; i < 10; i++) {
-                    newCharDate[i] = charDate[i+4];
-                }
-                getDateOnly = String.valueOf(newCharDate);
+        if (lastIdResep != null) {
+            char[] charDate = lastIdResep.toCharArray();
+            char[] newCharDate = new char[10];
+            for (int i = 0; i < 10; i++) {
+                newCharDate[i] = charDate[i + 4];
             }
-            if (lastIdResep == null || (!getDateNow().equals(getDateOnly))) {
-                idResepFix = awalan.concat("001");
+            getDateOnly = String.valueOf(newCharDate);
+        }
+        if (lastIdResep == null || (!getDateNow().equals(getDateOnly))) {
+            idResepFix = awalan.concat("001");
+        } 
+        else {
+            char[] lastDigit = lastIdResep.toCharArray();
+            char[] newLastDigit = new char[3];
+            for (int i = 0; i < 3; i++) {
+                newLastDigit[i] = lastDigit[i + 15];
             }
-            else{
-                char[] lastDigit = lastIdResep.toCharArray();
-                char[] newLastDigit = new char[3];
-                for (int i = 0; i < 3; i++) {
-                    newLastDigit[i] = lastDigit[i+15];
-                }
-                int lastDigitIdDetail = Integer.parseInt(String.valueOf(newLastDigit))+1;
-                if (lastDigitIdDetail < 10) {
-                    idResepFix = awalan.concat("00").concat(String.valueOf(lastDigitIdDetail));
-                }
-                else if (lastDigitIdDetail >= 10 && lastDigitIdDetail < 100){
-                    idResepFix = awalan.concat("0").concat(String.valueOf(lastDigitIdDetail));
-                }
-                else if (lastDigitIdDetail >= 100){
-                    idResepFix = awalan.concat(String.valueOf(lastDigitIdDetail));
-                }
+            int lastDigitIdDetail = Integer.parseInt(String.valueOf(newLastDigit)) + 1;
+            if (lastDigitIdDetail < 10) {
+                idResepFix = awalan.concat("00").concat(String.valueOf(lastDigitIdDetail));
+            } else if (lastDigitIdDetail >= 10 && lastDigitIdDetail < 100) {
+                idResepFix = awalan.concat("0").concat(String.valueOf(lastDigitIdDetail));
+            } else if (lastDigitIdDetail >= 100) {
+                idResepFix = awalan.concat(String.valueOf(lastDigitIdDetail));
             }
+        }
         return idResepFix;
     }
-    
-    public String getIdRekamMedis()throws RemoteException{
+
+    public String getIdRekamMedis() throws RemoteException {
         String lastIdRekamMedik = rekamMedikService.getlastIdRekamMedik();
         String[] splitDateNow = getDateNow().split("-");
         String dateNow = splitDateNow[0].concat(splitDateNow[1]).concat(splitDateNow[2]);
@@ -178,72 +189,104 @@ public class IsiRekamMedisController {
         //REKMED-20150418-001
         String idRekamMedisFix = " ";
         String getDateOnly = " ";
-            if (lastIdRekamMedik != null) {
-                char[] charDate = lastIdRekamMedik.toCharArray();
-                char[] newCharDate = new char[8];
-                for (int i = 0; i < 8; i++) {
-                    newCharDate[i] = charDate[i+7];
-                }
-                getDateOnly = String.valueOf(newCharDate);
+        if (lastIdRekamMedik != null) {
+            char[] charDate = lastIdRekamMedik.toCharArray();
+            char[] newCharDate = new char[8];
+            for (int i = 0; i < 8; i++) {
+                newCharDate[i] = charDate[i + 7];
             }
-            if (lastIdRekamMedik == null || (!dateNow.equals(getDateOnly))) {
-                idRekamMedisFix = awalan.concat("001");
+            getDateOnly = String.valueOf(newCharDate);
+        }
+        if (lastIdRekamMedik == null || (!dateNow.equals(getDateOnly))) {
+            idRekamMedisFix = awalan.concat("001");
+        } 
+        else {
+            char[] lastDigit = lastIdRekamMedik.toCharArray();
+            char[] newLastDigit = new char[3];
+            for (int i = 0; i < 3; i++) {
+                newLastDigit[i] = lastDigit[i + 16];
             }
-            else{
-                System.out.println("masuk else");
-                char[] lastDigit = lastIdRekamMedik.toCharArray();
-                char[] newLastDigit = new char[3];
-                for (int i = 0; i < 3; i++) {
-                    newLastDigit[i] = lastDigit[i+16];
-                }
-                int lastDigitIdDetail = Integer.parseInt(String.valueOf(newLastDigit))+1;
-                if (lastDigitIdDetail < 10) {
-                    idRekamMedisFix = awalan.concat("00").concat(String.valueOf(lastDigitIdDetail));
-                }
-                else if (lastDigitIdDetail >= 10 && lastDigitIdDetail < 100){
-                    idRekamMedisFix = awalan.concat("0").concat(String.valueOf(lastDigitIdDetail));
-                }
-                else if (lastDigitIdDetail >= 100){
-                    idRekamMedisFix = awalan.concat(String.valueOf(lastDigitIdDetail));
-                }
+            int lastDigitIdDetail = Integer.parseInt(String.valueOf(newLastDigit)) + 1;
+            if (lastDigitIdDetail < 10) {
+                idRekamMedisFix = awalan.concat("00").concat(String.valueOf(lastDigitIdDetail));
+            } else if (lastDigitIdDetail >= 10 && lastDigitIdDetail < 100) {
+                idRekamMedisFix = awalan.concat("0").concat(String.valueOf(lastDigitIdDetail));
+            } else if (lastDigitIdDetail >= 100) {
+                idRekamMedisFix = awalan.concat(String.valueOf(lastDigitIdDetail));
             }
+        }
         return idRekamMedisFix;
     }
     
-    public void getNamaObat(IsiResep ui)throws RemoteException{
+    public String getIdKunjungan() throws RemoteException{
+        String lastIdKunjungan = kunjunganService.getLastIdKunjungan();
+        String[] splitDateNow = getDateNow().split("-");
+        String dateNow = splitDateNow[0].concat(splitDateNow[1]).concat(splitDateNow[2]);
+        String awalan = "VIS-".concat(dateNow).concat("-");
+        // VIS-20150420-001
+        String idKunjunganFix = " ";
+        String getDateOnly = " ";
+        if (lastIdKunjungan != null) {
+            char[] charDate = lastIdKunjungan.toCharArray();
+            char[] newCharDate = new char[8];
+            for (int i = 0; i < 8; i++) {
+                newCharDate[i] = charDate[i + 4];
+            }
+            getDateOnly = String.valueOf(newCharDate);
+        }
+        if (lastIdKunjungan == null || !dateNow.equals(getDateOnly)) {
+            idKunjunganFix = awalan.concat("001");
+        }
+        else {
+            char[] lastDigit = lastIdKunjungan.toCharArray();
+            char[] newLastDigit = new char[3];
+            for (int i = 0; i < 3; i++) {
+                newLastDigit[i] = lastDigit[i + 13];
+            }
+            int lastDigitIdDetail = Integer.parseInt(String.valueOf(newLastDigit)) + 1;
+            if (lastDigitIdDetail < 10) {
+                idKunjunganFix = awalan.concat("00").concat(String.valueOf(lastDigitIdDetail));
+            } else if (lastDigitIdDetail >= 10 && lastDigitIdDetail < 100) {
+                idKunjunganFix = awalan.concat("0").concat(String.valueOf(lastDigitIdDetail));
+            } else if (lastDigitIdDetail >= 100) {
+                idKunjunganFix = awalan.concat(String.valueOf(lastDigitIdDetail));
+            }
+        }
+        return idKunjunganFix;
+    }
+
+    public void getNamaObat(IsiResep ui) throws RemoteException {
         List<Obat> list = new ArrayList<Obat>();
         list = obatService.getObat();
-        for (int i = 0; i < list.size(); i++) {            
+        for (int i = 0; i < list.size(); i++) {
             ui.comboBoxObat.addItem(list.get(i).getNamaObat());
-        }               
+        }
     }
-    
-    public void getNamaPenyakit(isirekammedis ui) throws RemoteException{
+
+    public void getNamaPenyakit(isirekammedis ui) throws RemoteException {
         List<Penyakit> list = new ArrayList<Penyakit>();
         list = penyakitService.getPenyakit();
-        for (int i = 0; i < list.size(); i++) {            
+        for (int i = 0; i < list.size(); i++) {
             ui.comboBoxPenyakit.addItem(list.get(i).getIdPenyakit() + " " + list.get(i).getNamaPenyakit());
         }
     }
-    
-    public String[] getIdDokter(String userName) throws RemoteException{
+
+    public String[] getIdDokter(String userName) throws RemoteException {
         String[] dokterInfor = dokterService.getIdNamaDokter(userName);
         return dokterInfor;
     }
-    
-    public boolean cekIdPasien(String idPasien) throws RemoteException{
+
+    public boolean cekIdPasien(String idPasien) throws RemoteException {
         boolean registered = false;
-        Pasien pasien = pasienService.getPasien(idPasien);
-        if (String.valueOf(pasien.getIdPasien()).equals(idPasien)) {
+        if (pasienService.getPasien(idPasien) != null) {
             registered = true;
         }
         return registered;
     }
-    
-    public String getDateNow() throws RemoteException{
+
+    public String getDateNow() throws RemoteException {
         DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
         String date = df.format(new java.util.Date());
         return date;
     }
-
 }
