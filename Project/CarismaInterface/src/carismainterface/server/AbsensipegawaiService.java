dba3c609747f5af50fda435441cdf9eb0b70c706
Franzvilosya <franz_rizqi@yahoo.com>
2015-04-25 20:44:33 +0700

    Server - Add missing service for absensipegawai
    Do : nambahin kelas interface dan service buat absensipegawai
    Hambatan : -
    To Do : maintenance server

---
+++ Project/CarismaInterface/src/carismainterface/server/AbsensipegawaiService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package carismainterface.server;
+
+import carismainterface.entity.Absensipegawai;
+import java.rmi.RemoteException;
+import java.util.List;
+
+/**
+ *
+ * @author kepoterz
+ */
+public interface AbsensipegawaiService {
+    void insertAbsensipegawai(Absensipegawai absensipegawai) throws RemoteException;
+
+    void updateAbsensipegawai(Absensipegawai absensipegawai) throws RemoteException;
+
+    void deleteAbsensipegawai(String idabsensipegawai) throws RemoteException;
+
+    Absensipegawai getAbsensipegawai(String pegawaiidpegawai) throws RemoteException;
+
+    List<Absensipegawai> getAbsensipegawai() throws RemoteException;
+}
