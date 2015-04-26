d69aa25fbbd39f74e22c37a722c41f62e521148b
devanastiko <devan.astiko@gmail.com>
2015-04-26 19:30:32 +0700

    Do re-Design View 404 Notofound / Eror Page Hambatan :tidak ada To DO : direktori penyakit dan penjelasannya pada web

--- Project/carisma.web/controllers/error.php
+++ Project/carisma.web/controllers/error.php
@@ -1,12 +1,38 @@
 <?php
 
-class error extends Controller{
+class error extends Controller {
+    
     function __construct() {
         parent::__construct();
         Session::init();
-         $this->view->render('error/index', 0, 0);
+            
+       $masukindums = "";
+        $masukindums = $masukindums .'<div id="contact" class="contact">
+    <div class="map">
+        <div class="contact-info">
+            <div class="container">
+                <div class="contact-grids">
+                    <div class="col-md-7 contact-grid-right">
+                        <h3>404 Not Found</h3> 
+                        <h4> The requested URL 404 was not found on site.</h4>
+                        <div class="clearfix"> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>';
+        $this->view->ini = $masukindums;
+        $this->view->title = "Home";
+        $this->view->render('error/index', 1, 0);  
+    
+        
     } 
+ 
+    function index(){
+        Index::__construct();
+    }
+      
     
-    
-    
+   
 }
--- Project/carisma.web/views/error/index.php
+++ Project/carisma.web/views/error/index.php
@@ -1,3 +1,4 @@
-<?php
+<?php 
 
-echo 'hayooo mau ngapain';
+echo $this->ini
+?>
