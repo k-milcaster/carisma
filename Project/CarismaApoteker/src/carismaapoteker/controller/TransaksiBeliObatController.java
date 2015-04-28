3211c22188ee1cae7b7365765f0f3889a11e57f7
hanamahrifah <hanamahrifah@gmail.com>
2015-04-28 21:26:27 +0700

    Do : Nyicil Get TransaksiBeliObat Controller Hambatan : - To Do : Dilanjutin lagi

--- Project/CarismaApoteker/src/carismaapoteker/controller/TransaksiBeliObatController.java
+++ Project/CarismaApoteker/src/carismaapoteker/controller/TransaksiBeliObatController.java
@@ -6,9 +6,13 @@
 
 package carismaapoteker.controller;
 
+import carismaapoteker.boundaries.TransaksiBeliObat;
 import carismainterface.entity.Transaksibeliobat;
 import carismainterface.server.TransaksibeliobatService;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -23,6 +27,19 @@ public class TransaksiBeliObatController {
         this.transaksibeliobat = client.getTransaksibeliobatService();
     }
     
+    public DefaultTableModel getTransaksiBeliObat(TransaksiBeliObat ui){
+    List<Transaksibeliobat> list = new ArrayList<Transaksibeliobat>();
+    DefaultTableModel model = new DefaultTableModel();
+    model.addColumn("Id Transaksi Beli Obat");
+    model.addColumn("Tanggal Beli Obat");
+    model.addColumn("Keterangan");
+    model.addColumn("Harga Beli Obat");
+    for(int i = 0;i<list.size();i++){
+        model.addRow(new Object []{list.get(i).getIdTransaksibeliobat(),list.get(i).getDateTransaksibeli(),list.get(i).getKeterangan(),list.get(i).getHargabeliObat()});
+        System.out.println("get Transaksi beli obat");
+    }
+    return model;
+    }
 //    
 //    public void insertTransaksiBeliObat(carismaapoteker.boundaries.TransaksiBeliObat ui){
 //        
