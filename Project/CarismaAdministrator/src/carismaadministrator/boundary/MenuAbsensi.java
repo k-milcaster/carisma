c15f244a7c8024609b6e51c72c133a5bc6b3600b
Fitriana Dzulfaidah <fitrianadz94@gmail.com>
2015-05-04 19:10:31 +0700

    Do : menyelesaikan story card absensi pegawai dan absensi dokter hambatan : tidak ada To Do : melakukan revisi jika ada error

---
+++ Project/CarismaAdministrator/src/carismaadministrator/boundary/MenuAbsensi.java
@@ -0,0 +1,146 @@
+package carismaadministrator.boundary;
+
+import carismaadministrator.controller.AbsensiController;
+import carismaadministrator.controller.ClientSocket;
+import carismainterface.server.UserService;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MenuAbsensi extends javax.swing.JFrame {
+    
+    private AbsensiController absensiController;
+    private ClientSocket clientSocket;
+    private UserService login;
+    private String userName;
+    String[] namaPegawai;
+    public MenuAbsensi(ClientSocket client, String userName) throws RemoteException{
+        clientSocket = client;
+        this.userName = userName;
+        login = clientSocket.getUserService();
+        initComponents();
+        absensiController = new AbsensiController(clientSocket);
+        namaPegawai = absensiController.getNamaPegawai(this.userName);
+        labelAdministrator.setText(namaPegawai[0]);
+        setLocationRelativeTo(this);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        labelAdministrator = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jFormattedTextField1 = new javax.swing.JFormattedTextField();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Administrator", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Agency FB", 1, 14))); // NOI18N
+
+        labelAdministrator.setFont(new java.awt.Font("Agency FB", 1, 24)); // NOI18N
+        labelAdministrator.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
+
+        org.jdesktop.layout.GroupLayout jPanel1Layout = new org.jdesktop.layout.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(labelAdministrator, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 200, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel1Layout.createSequentialGroup()
+                .add(labelAdministrator, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 29, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(0, 0, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, -1, -1));
+
+        jButton1.setFont(new java.awt.Font("AvantGarde Bk BT", 1, 14)); // NOI18N
+        jButton1.setText("Absensi Dokter");
+        jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 0, 51)));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 230, 150, 40));
+
+        jButton2.setFont(new java.awt.Font("AvantGarde Bk BT", 1, 14)); // NOI18N
+        jButton2.setText("Absensi Pegawai");
+        jButton2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 0, 51)));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 300, 150, 40));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Tanggal", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Agency FB", 1, 14))); // NOI18N
+
+        jFormattedTextField1.setEditable(false);
+        jFormattedTextField1.setBackground(new java.awt.Color(255, 255, 255));
+        jFormattedTextField1.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("yyyy-MM-dd"))));
+        jFormattedTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jFormattedTextField1.setFont(new java.awt.Font("Agency FB", 1, 18)); // NOI18N
+        jFormattedTextField1.setValue(new java.util.Date());
+
+        org.jdesktop.layout.GroupLayout jPanel3Layout = new org.jdesktop.layout.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel3Layout.createSequentialGroup()
+                .add(0, 0, Short.MAX_VALUE)
+                .add(jFormattedTextField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 171, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel3Layout.createSequentialGroup()
+                .add(jFormattedTextField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(0, 1, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 50, -1, -1));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/carismaadministrator/image/Menu Absensi.jpg"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 500));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            new AbsensiDokter(clientSocket, namaPegawai[0]).show();
+        } catch (RemoteException ex) {
+            Logger.getLogger(MenuAbsensi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try {
+            new AbsensiPegawai(clientSocket, namaPegawai[0]).show();
+        } catch (RemoteException ex) {
+            Logger.getLogger(MenuAbsensi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    public static void main(String args[]) {
+
+       
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JFormattedTextField jFormattedTextField1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JLabel labelAdministrator;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ Project/CarismaAdministrator/src/carismaadministrator/controller/AbsensiController.java
@@ -0,0 +1,97 @@
+package carismaadministrator.controller;
+
+import carismaadministrator.boundary.*;
+import carismainterface.entity.*;
+import carismainterface.server.*;
+import java.util.*;
+import java.rmi.RemoteException;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class AbsensiController {
+
+    private AbsensidokterService absensiDokterService;
+    private AbsensipegawaiService absensiPegawaiService;
+    private DokterService dokterService;
+    private PegawaiService pegawaiService;
+
+    public AbsensiController(ClientSocket client) throws RemoteException {
+        absensiDokterService = client.getAbsensiDokterService();
+        absensiPegawaiService = client.getAbsensiPegawaiService();
+        dokterService = client.getDokterService();
+        pegawaiService = client.getPegawaiService();
+    }
+
+    public void insertAbsensiDokter(String idDokter, String kehadiranReguler, String kehadiranLembur, String tanggalKehadiran) throws RemoteException {
+        Absensidokter absensiDokter = new Absensidokter();
+        absensiDokter.setDokterIdDokter(idDokter);
+        absensiDokter.setKehadiranregulerDokter(kehadiranReguler);
+        absensiDokter.setKehadiranlemburDokter(kehadiranLembur);
+        absensiDokter.setDateAbsensidokter(tanggalKehadiran);
+        absensiDokterService.insertAbsensidokter(absensiDokter);
+    }
+
+    public void deleteAbsensiDokter(String idDokter) throws RemoteException{
+        absensiDokterService.deleteAbsensidokter(idDokter);
+    }
+
+    public void insertAbsensiPegawai(String idPegawai, String kehadiranReguler, String kehadiranLembur, String tanggalKehadiran) throws RemoteException {
+        Absensipegawai absensiPegawai = new Absensipegawai();
+        absensiPegawai.setPegawaiIdPegawai(idPegawai);
+        absensiPegawai.setKehadiranregulerPegawai(kehadiranReguler);
+        absensiPegawai.setKehadiranlemburPegawai(kehadiranLembur);
+        absensiPegawai.setDateAbsensipegawai(tanggalKehadiran);
+        absensiPegawaiService.insertAbsensipegawai(absensiPegawai);
+    }
+    
+    public DefaultComboBoxModel getIdDokter() throws RemoteException {
+        DefaultComboBoxModel idDokter = new DefaultComboBoxModel();
+        List<Dokter> listIdDokter = new ArrayList<Dokter>();
+        listIdDokter = dokterService.getDokter();
+        for (int i = 0; i < listIdDokter.size(); i++) {
+            idDokter.addElement(listIdDokter.get(i).getIdDokter() + " " + listIdDokter.get(i).getNamaDokter());
+        }
+        return idDokter;
+    }
+
+    public DefaultTableModel getTabelAbsensiDokter() throws RemoteException {
+        DefaultTableModel tabel = new DefaultTableModel();
+        tabel.addColumn("Id Dokter");
+        tabel.addColumn("Kehadiran Reguler");
+        tabel.addColumn("Kehadiran Lembur");
+        List<Absensidokter> listAbsensiDokter = new ArrayList<Absensidokter>();
+        listAbsensiDokter = absensiDokterService.getAbsensidokter();
+        for (int i = 0; i < listAbsensiDokter.size(); i++) {
+            tabel.addRow(new Object[]{listAbsensiDokter.get(i).getDokterIdDokter(), listAbsensiDokter.get(i).getKehadiranregulerDokter(), listAbsensiDokter.get(i).getKehadiranlemburDokter()});
+        }
+        return tabel;
+    }
+
+    public DefaultComboBoxModel getIdPegawai() throws RemoteException {
+        DefaultComboBoxModel idPegawai = new DefaultComboBoxModel();
+        List<Pegawai> listIdPegawai = new ArrayList<Pegawai>();
+        listIdPegawai = pegawaiService.getPegawai();
+        for (int i = 0; i < listIdPegawai.size(); i++) {
+            idPegawai.addElement(listIdPegawai.get(i).getIdPegawai() + " " + listIdPegawai.get(i).getNamaPegawai());
+        }
+        return idPegawai;
+    }
+    
+    public DefaultTableModel getTabelAbsensiPegawai() throws RemoteException {
+        DefaultTableModel tabel = new DefaultTableModel();
+        tabel.addColumn("Id Pegawai");
+        tabel.addColumn("Kehadiran Reguler");
+        tabel.addColumn("Kehadiran Lembur");
+        List<Absensipegawai> listAbsensiDokter = new ArrayList<Absensipegawai>();
+        listAbsensiDokter = absensiPegawaiService.getAbsensipegawai();
+        for (int i = 0; i < listAbsensiDokter.size(); i++) {
+            tabel.addRow(new Object[]{listAbsensiDokter.get(i).getPegawaiIdPegawai(), listAbsensiDokter.get(i).getKehadiranregulerPegawai(), listAbsensiDokter.get(i).getKehadiranlemburPegawai()});
+        }
+        return tabel;
+    }
+
+    public String[] getNamaPegawai(String username) throws RemoteException {
+        String[] namaPegawai = pegawaiService.getIdNamaPegawai(username);
+        return namaPegawai;
+    }
+}
