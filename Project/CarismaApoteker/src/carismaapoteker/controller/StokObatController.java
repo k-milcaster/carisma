a7522dda63ec5ad05cb90fc91e467c4b621a60f4
hanamahrifah <hanamahrifah@gmail.com>
2015-05-11 19:59:50 +0700

    Do : Menampilkan nama apoteker controller Hambatan : - To Do : -

--- Project/CarismaApoteker/src/carismaapoteker/controller/StokObatController.java
+++ Project/CarismaApoteker/src/carismaapoteker/controller/StokObatController.java
@@ -10,6 +10,7 @@ import carismainterface.entity.Detailobat;
 import carismainterface.entity.Obat;
 import carismainterface.server.DetailobatService;
 import carismainterface.server.ObatService;
+import carismainterface.server.PegawaiService;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,13 +24,15 @@ public class StokObatController {
 
     private ObatService obatService;
     private DetailobatService detailobatService;
-
+    private PegawaiService pegawaiService;
+    
     public StokObatController(ClientSocket client) throws RemoteException {
         this.detailobatService = client.getDetailonatSerivice();
         this.obatService = client.getObatService();
+        this.pegawaiService = client.getPegawaiService();
     }
 
-    public DefaultTableModel getObats(StokObat ui) throws RemoteException {
+    public DefaultTableModel getObats() throws RemoteException {
         List<Obat> list = new ArrayList<Obat>();
         list = obatService.getObat();
         DefaultTableModel model = new DefaultTableModel();
@@ -43,7 +46,7 @@ public class StokObatController {
         for (int i = 0; i < list.size(); i++) {
             model.addRow(new Object[]{list.get(i).getIdObat(), list.get(i).getNamaObat(), list.get(i).getQtyObat(), list.get(i).getJenisObat(), list.get(i).getKeterangan(), list.get(i).getHargajualObat(), list.get(i).getStokkritisObat()});
         }
-        ui.tableMedicine.setModel(model);
+       // ui.tableMedicine.setModel(model);
         return model;
     }
 
@@ -93,6 +96,10 @@ public class StokObatController {
         }
         return model;
 
+    }
+    public String[] getNamaPegawai (String username) throws RemoteException{
+        String[] namaApoteker = pegawaiService.getIdNamaPegawai(username);
+        return namaApoteker;
     }
 
 }
