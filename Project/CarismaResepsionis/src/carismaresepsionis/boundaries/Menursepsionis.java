cff84c198be9c520e34d66aaa0ed6ed189dd5130
AdindaMustika <adindaheni@gmail.com>
2015-05-15 17:00:05 +0700

    Do : Edit antrian controller

--- Project/CarismaResepsionis/src/carismaresepsionis/boundaries/Menursepsionis.java
+++ Project/CarismaResepsionis/src/carismaresepsionis/boundaries/Menursepsionis.java
@@ -32,7 +32,8 @@ public class Menursepsionis extends javax.swing.JFrame {
         AntrianController control = new AntrianController(this.client);
         ps = client.getPasienService();
         initComponents();
-        control.getAntrian(this);       
+        tableAntri = control.getAntrian();
+        tableDaftarAntrian.setModel(tableAntri);
         this.setExtendedState(this.MAXIMIZED_BOTH);
         Namanya.setEditable(false);
         Namanya.setText(String.valueOf(this.userName));
