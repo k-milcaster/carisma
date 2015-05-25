2bbfa3518664c4eee52681d8a60a51177ffbcafc
devanastiko <devan.astiko@gmail.com>
2015-05-25 22:16:45 +0700

    Do Single Deskripsi Penyakit (Done) To Do : fixing Images and Content Hambatan :

--- Project/carisma.web/controllers/penyakit.php
+++ Project/carisma.web/controllers/penyakit.php
@@ -1,13 +1,13 @@
 <?php
 
 class penyakit extends Controller {
-    
+
     function __construct() {
         parent::__construct();
         Session::init();
-            
-       $masukindums = "";
-        $masukindums = $masukindums .'<div id="contact" class="contact">
+
+        $masukindums = "";
+        $masukindums = $masukindums . '<div id="contact" class="contact">
     <div class="map">
         <div class="contact-info">
             <div class="container">
@@ -25,34 +25,25 @@ class penyakit extends Controller {
     </div>
 </div>';
         $masukindums = "";
-        $masukindums = $masukindums .'<div id="contact" class="contact">
+        $masukindums = $masukindums . '<div id="contact" class="contact">
     <div class="map">
         <div class="contact-info">
             <div class="container">
                 <div class="contact-grids">
                 
-                    <div class="col-md-7 contact-grid-right">
-                    <h3>Cari Penyakit</h3>
-<br>                
-                    <input type="text" name="cari">
-                        <input type="submit" value="CARI PENYAKIT" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>';
+              ';
         $this->view->ini = $masukindums;
         $this->view->title = "Home";
-        $this->view->render('penyakit/index', 1, 0);  
-    
-        
-    } 
- 
-    function index(){
+        $this->view->render('penyakit/index', 1, 0);
+    }
+
+    function index() {
         Index::__construct();
     }
-      
+
+    
+
+    
     
    
 }
--- Project/carisma.web/views/penyakit/detail.php
+++ Project/carisma.web/views/penyakit/detail.php
@@ -1,3 +1,46 @@
+
+
+
 <div id="about" class="about">
-     <?php echo $this->ini ?>
-</div>
+    <div class="container">
+        <div class="header about-header text-center">
+            <h2>Deskripsi Penyakit </h2>
+            
+        </div>
+        <!---- About-grids ---->
+        <div class="about-grids">
+            <div class="col-md-4">
+                <div class="about-grid n-about-grid n-about-grid1">
+                    <img src="<?php echo URL; ?>public/images/img3.jpg" title="name" />
+                    <span class="t-icon1"> </span>
+                    <div class="about-grid-info text-center">
+                        <h3><a href="https://www.facebook.com/indra.kharisma.r"><?php
+                echo  Session::get('id');
+                ?></a></h3>
+                        <p>Deskripsi Penyakit <?php echo $this->judul ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                
+                
+                    <div class="about-grid-info text-center">
+                        <table >
+                            <tr>
+                                <td>
+                                   <?php echo $this->judul ?>
+                                </td>
+                                
+                            </tr>
+                            
+<?php echo $this->isi ?> 
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!--</div>-->
+
+
+            <!---- About-grids ---->
+        </div>
+</div>    <!---- about ---->
