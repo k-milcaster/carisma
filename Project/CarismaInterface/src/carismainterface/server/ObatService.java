a17c198be58af15e69df0c8a15b8d044f2a10302
sintaaa <sintaekaa@gmail.com>
2015-05-29 14:09:55 +0700

    Do : bikin objek deleteObat Hambatan : - To do : sudah selesai

--- Project/CarismaInterface/src/carismainterface/server/ObatService.java
+++ Project/CarismaInterface/src/carismainterface/server/ObatService.java
@@ -14,7 +14,7 @@ public interface ObatService extends Remote {
 
     boolean updateObat(Obat obat) throws RemoteException;
 
-    boolean deleteObat(int idobat) throws RemoteException;
+    boolean deleteObat(String idobat) throws RemoteException;
 
     Obat getObat(int idobat) throws RemoteException;
     
@@ -25,6 +25,5 @@ public interface ObatService extends Remote {
     int getLastIdObat() throws RemoteException;
     
     boolean updateQtyObat(int id, int qty) throws RemoteException;
-    
-    int cekStokObat(int idObat)throws RemoteException;
+  
 }
