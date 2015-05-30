1186607f20ef87a5b4b2bbd0c5455f258403e961
K-MiL Caster <k.cas7er@gmail.com>
2015-05-31 00:30:08 +0700

    Update Antrian Online
    Do : Nyicil antrian online
    Hambatan : kangen pacar
    To Do : finishing

--- Project/carisma.web/controllers/book.php
+++ Project/carisma.web/controllers/book.php
@@ -5,6 +5,14 @@ class Book extends Controller {
     function __construct() {
         parent::__construct();
         Session::init();
+        if (Session::get('id') != NULL) {
+            
+        } else {
+            $this->view->ini = "Anda perlu log in untuk booking antrian";
+            $this->view->title = "Login";
+            $this->view->render('login/index', 1, 0);
+            die();
+        }
         //$this->index();
     }
 
@@ -49,38 +57,50 @@ class Book extends Controller {
     }
 
     function doBook() {
-        $this->loadModel('Login');
-        $temp = $this->model->getIdUser(Session::get('id'));
-        $iduser = '';
-        foreach ($temp as $key => $value) {
-            $iduser = $value[0];
-        }
-        $tgl = $_POST['av'];
         $idDokter = $_POST['idDokter'];
-        $this->loadModel('antrian');
-        $getCount = $this->model->getCountAntrian();
-        $cek = $this->model->cekOnce($iduser, $idDokter, $tgl);
-        if ($cek) {
-            foreach ($getCount as $key => $value) {
-                $count = $value[0];
-            }
-            $count = $count + 1;
-            if ($count < 10) {
-                $id = 'O00' . $count;
-            } else if ($count >= 10) {
-                $id = 'O0' . $count;
-            } else {
-                $id = 'O' . $count;
-            }
-            $temp = $this->model->getLastNomorAntrian();
-            foreach ($temp as $key => $value) {
-                $noAntrian = $value[0];
-            }
-            $noAntrian = $noAntrian + 1;
-            $this->model->insertAntrian($id, $iduser, $idDokter, $noAntrian, 'ONLINE', $tgl);
-            echo 'berhasil';
+
+        if (!isset($_POST['av'])) {
+            echo '<script language="javascript">';
+            echo 'alert("Anda belum memilih hari")';
+            echo '</script>';
+            Redirect::go('http://localhost/carisma.web/book/form/' . $idDokter);
         } else {
-            echo 'gagal';
+            $tgl = $_POST['av'];
+            $this->loadModel('Login');
+            $temp = $this->model->getIdUser(Session::get('id'));
+            $iduser = '';
+            foreach ($temp as $key => $value) {
+                $iduser = $value[0];
+            }
+            $this->loadModel('antrian');
+            $getCount = $this->model->getCountAntrian();
+            $cek = $this->model->cekOnce($iduser, $idDokter, $tgl);
+            if ($cek) {
+                foreach ($getCount as $key => $value) {
+                    $count = $value[0];
+                }
+                $count = $count + 1;
+                if ($count < 10) {
+                    $id = 'O00' . $count;
+                } else if ($count >= 10) {
+                    $id = 'O0' . $count;
+                } else {
+                    $id = 'O' . $count;
+                }
+                $temp = $this->model->getLastNomorAntrian();
+                foreach ($temp as $key => $value) {
+                    $noAntrian = $value[0];
+                }
+                $noAntrian = $noAntrian + 1;
+                Redirect::notif($idDokter);
+                //$this->model->insertAntrian($id, $iduser, $idDokter, $noAntrian, 'ONLINE', $tgl);
+                
+            } else {
+                echo '<script language="javascript">';
+                echo 'alert("Anda sudah antri pada hari tersebut")';
+                echo '</script>';
+                Redirect::go('http://localhost/carisma.web/book/form/' . $idDokter);
+            }
         }
     }
 
