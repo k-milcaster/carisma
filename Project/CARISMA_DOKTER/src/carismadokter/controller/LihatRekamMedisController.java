c5680273de90d73ef3159377abc00d895319afea
Anes Dirsi Eka Permatasari <Annesdirsy@gmail.com>
2015-06-02 10:31:46 +0700

    do: Revisi gui dokterlihatrekammedis.form hambatan: tidak ada to do: Menyelesaikan dokter lihat rekam medik

--- Project/CARISMA_DOKTER/src/carismadokter/controller/LihatRekamMedisController.java
+++ Project/CARISMA_DOKTER/src/carismadokter/controller/LihatRekamMedisController.java
@@ -3,8 +3,12 @@
  * and open the template in the editor.
  */
 package carismadokter.controller;
+import carismainterface.entity.Dokter;
 import carismainterface.entity.Pasien;
+import carismainterface.entity.Rekammedik;
+import carismainterface.server.DokterService;
 import carismainterface.server.PasienService;
+import carismainterface.server.PoliService;
 import carismainterface.server.RekammedikService;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
@@ -17,43 +21,47 @@ import javax.swing.table.DefaultTableModel;
 public class LihatRekamMedisController {
     
     private PasienService pasienService;
+    private DokterService dokterService;
     private RekammedikService rekamMedik;
     
     public LihatRekamMedisController(ClientSocket client) throws RemoteException {
         this.pasienService = client.getPasienService();
         this.rekamMedik = client.getRekamMedikService();
+        this.dokterService = client.getDokterService();     
     }
     
-    public DefaultTableModel getPasienList() throws RemoteException {
-        List<Pasien> list = new ArrayList<Pasien>();
-        list = pasienService.getPasien();
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("ID Pasien");
-        model.addColumn("Nama Pasien");
-        for (int i = 0; i < list.size(); i++) {
-            model.addRow(new Object[]{list.get(i).getIdPasien(), list.get(i).getNamaPasien()});
-        }
-        //ui.TabelPasien.setModel(model);
-        return model;
+    public Pasien getDetailPasien(String idPasien) throws RemoteException {
+        Pasien pasien = pasienService.getPasien(idPasien);
+        return pasien;
     }
     
-     public DefaultTableModel getPasienbyName (String nama) throws RemoteException{
-        List<Pasien> list = new ArrayList<Pasien>();
-        list = pasienService.getPasienByName(nama);
+    public Dokter getDokterDetail(String idDokter) throws RemoteException {
+        Dokter dokter = dokterService.getDokter(idDokter);
+        return dokter;
+    }
+    
+    public DefaultTableModel getRekamMedik() throws RemoteException {
+        List<Rekammedik> list = new ArrayList<Rekammedik>();
+        list = rekamMedik.getRekamMedik();
         DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Id RekamMedik");
+        model.addColumn("Id Dokter");
         model.addColumn("Id Pasien");
-        model.addColumn("Nama Pasien");
+        model.addColumn("Tgl Pemeriksaan");
+        model.addColumn("Keluhan");
+        model.addColumn("Pemeriksaan Dokter");
+        model.addColumn("Terapi");
+        model.addColumn("Alergi Obat");
+        model.addColumn("Kesimpulan");
+        model.addColumn("Kondisi Pasien Keluar");
+        model.addColumn("Id Resep");
         
         for (int i = 0; i < list.size(); i++) {
-            model.addRow(new Object[]{list.get(i).getIdPasien(), list.get(i).getNamaPasien()});
+            model.addRow(new Object[]{list.get(i).getIdRekammedik(), list.get(i).getDokterIdDokter(), list.get(i).getPasienIdPasien(), list.get(i).getTglRekammedik(), list.get(i).getKeluhanRekammedik(), list.get(i).getPemeriksaanRekammedik(), list.get(i).getTerapiRekammedik(), list.get(i).getAlergiobatRekammedik(), list.get(i).getKesimpulanRekammedis(), list.get(i).getKondisipasienkeluarRekammedis(), list.get(i).getResepIdResep() });
         }
+        //ui.TabelRekammedik.setModel(model);
         return model;
-        
-    }
-     
-//     public ArrayList getRekammedikById(String idpasien) throws RemoteException {
-//        ArrayList infoRekammedik = rekamMedik.getRekammedikById(idpasien);
-//        return infoRekammedik;
-//    }
+    }    
+
 } 
 
