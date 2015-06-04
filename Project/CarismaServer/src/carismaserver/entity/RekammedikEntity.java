c0f80129a798e930eec48b420ca3546ac4770c04
Fitriana Dzulfaidah <fitrianadz94@gmail.com>
2015-06-04 18:36:20 +0700

    Do : Mengubah Query getRekamMedikByPasien Hambatan : - To Do : -

--- Project/CarismaServer/src/carismaserver/entity/RekammedikEntity.java
+++ Project/CarismaServer/src/carismaserver/entity/RekammedikEntity.java
@@ -142,11 +142,14 @@ public class RekammedikEntity extends UnicastRemoteObject implements RekammedikS
         PreparedStatement statement = null;
         try {
             statement = DatabaseConnection.getConnection().prepareStatement(
-                    "SELECT * FROM rekammedik WHERE pasien_id_pasien = ?");
-            statement.setString(1, pasien);
+                    "SELECT rm.`id_rekammedik`, rm.`dokter_id_dokter`, d.nama_dokter, rm.`pasien_id_pasien`, "
+                    + "p.nama_pasien, rm.`tgl_rekammedik`, rm.`keluhan_rekammedik`, rm.`pemeriksaan_rekammedik`, "
+                    + "rm.`terapi_rekammedik`, rm.`alergiobat_rekammedik`, rm.`kesimpulan_rekammedis`, rm.`kondisipasienkeluar_rekammedis`, "
+                    + "rm.`resep_id_resep` FROM rekammedik AS rm, pasien AS p, dokter AS d WHERE rm.`pasien_id_pasien` = p.id_pasien "
+                    + "AND p.id_pasien = '"+pasien+"' GROUP BY rm.id_rekammedik");
             ResultSet result = statement.executeQuery();
             List<Rekammedik> rms = new ArrayList<Rekammedik>();
-            if (result.next()) {
+            while (result.next()) {
                 Rekammedik rm = new Rekammedik();
                 rm.setIdRekammedik(result.getString("id_rekammedik"));
                 rm.setDokterIdDokter(result.getString("dokter_id_dokter"));
